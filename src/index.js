4d09211e35b533beedd85a03eddd60773d9b74df
rasg <rodolfo@mutalis.com>
2019-07-19 22:32:30 -0500

    useEffect examples

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Counter() {
+  const [post, setPost] = useState(null)
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(res => res.json())
+      .then(data => {
+        console.log(post)
+        setPost(data)
+      })
+  }, [])
+
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  }, [count])
+
+  return (
+    <div>
+      <p>{JSON.stringify(post)}</p>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
